9c100e9dbda954a1e4cff59b51c868749648f12e
AdhilMohammed <114474267+adhilbayya@users.noreply.github.com>
2023-08-15 12:36:23 +0530

    Create ternaryOperator.js

---
+++ JavaScript/ternaryOperator.js
@@ -0,0 +1,7 @@
+let soup = "Chicken soup";
+let customerAccess = true;
+let soupAccess = customerAccess ? "No soup for you tdy":
+                 soup ? `Here is your ${soup}` : 
+                 "soup is Not available";
+
+console.log(soupAccess)
