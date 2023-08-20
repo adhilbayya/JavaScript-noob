f55ffb5c2b9ceb190744c0f378f601010399c322
AdhilMohammed <114474267+adhilbayya@users.noreply.github.com>
2023-08-20 20:18:24 +0530

    Create 2dimentional.js

---
+++ JavaScript/2dimentional.js
@@ -0,0 +1,22 @@
+const bestFootballPlayers1 = ["Messi","Maradona","Ronaldo","Pele"];
+const bestFootballPlayers2 = ["Neymar","Mbappe","Cruyif","Xavi"];
+
+
+const bestCricketPlayers1 = ["Virat","Sachin","Lara","Dhoni"];
+const bestCricketPlayers2 = ["Sangakara","Bumrah","Warne","Anderson"];
+
+
+// Single dimentional array.
+let GOAT = bestFootballPlayers1[0]; 
+console.log(GOAT);
+
+
+// Two dimentional array.
+const footballPlayers = [bestFootballPlayers1,bestFootballPlayers2];
+const cricketPlayers = [bestCricketPlayers1,bestCricketPlayers2]
+
+let bestFootballPlayer = footballPlayers[0][0];
+console.log(bestFootballPlayer)
+
+let bestCricketPlayer = cricketPlayers[0][1];
+console.log(bestCricketPlayer);
