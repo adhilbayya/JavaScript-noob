c9b6466b034dce47a50f8101943b2143a8f7c6b8
AdhilMohammed <114474267+adhilbayya@users.noreply.github.com>
2023-08-20 20:17:38 +0530

    Create 1dimentional.js

---
+++ JavaScript/1dimentional.js
@@ -0,0 +1,11 @@
+const bestFootballPlayers1 = ["Messi","Maradona","Ronaldo","Pele"];
+const bestFootballPlayers2 = ["Neymar","Mbappe","Cruyif","Xavi"];
+
+
+const bestCricketPlayers1 = ["Virat","Sachin","Lara","Dhoni"];
+const bestCricketPlayers2 = ["Sangakara","Bumrah","Warne","Anderson"];
+
+
+// Single dimentional array.
+let GOAT = bestFootballPlayers1[0]; 
+console.log(GOAT);
