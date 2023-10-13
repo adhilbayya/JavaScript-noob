26365f28cbef4a8c19500e77ae62db6a7523724a
AdhilMohammed <adhilvk445@gmail.com>
2023-10-13 07:39:18 +0530

    Update get-set2.js

--- JavaScript/class/get-set2.js
+++ JavaScript/class/get-set2.js
@@ -23,10 +23,10 @@ console.log(person.name) // Bayya
 
 class Person{
   constructor(name){
-    this.name = name;
+    this._name = name;
   }
   get name(){
-    return this.name;
+    return this._name;
   }
 }
 
