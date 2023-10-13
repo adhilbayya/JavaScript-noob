3f1abe1c844988b4961f7d655d0c6cadc85eead8
AdhilMohammed <adhilvk445@gmail.com>
2023-10-13 08:52:16 +0530

    Create getter-in-object.js

---
+++ JavaScript/object/getter-in-object.js
@@ -0,0 +1,15 @@
+let meeting = {
+  attendees : [],
+  add(attendee){
+    console.log(`${attendee} has joined the meeting`);
+    this.attendees.push(attendee);
+    return this;
+  },
+  get latest(){
+    let count = this.attendees.length;
+    return count == 0 ? undefined : attendees[count - 1];
+  }
+};
+
+meeting.add('Adhil').add('Raju').add('Richu');
+console.log(`${meeting.latest} is the latest attendee`);
