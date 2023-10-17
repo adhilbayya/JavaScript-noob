3f7fb51f42182889592b2209d4e0e3127b85027b
AdhilMohammed <adhilvk445@gmail.com>
2023-10-17 22:16:33 +0530

    Update sort1.js

--- JavaScript/function/sort1.js
+++ JavaScript/function/sort1.js
@@ -5,6 +5,11 @@ console.log(ans) //[1,12,2,3,4,6,7,9]
 
 let num = [2,5,3,8,6,1];
 let ans = num.sort(function(a, b){
-  return a - b;
+  return a - b;  //b - a -->for decending order
 });
 console.log(ans) //[ 1, 2, 3, 5, 6, 8 ]
+
+//other way (ES6)
+let num = [2,5,3,8,6,1];
+let ans = num.sort((a, b) => a-b);
+console.log(ans)
