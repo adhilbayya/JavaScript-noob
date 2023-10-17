1d606b25676ba7eec553cb04dea605779d04d90b
AdhilMohammed <adhilvk445@gmail.com>
2023-10-17 22:13:39 +0530

    Update sort1.js

--- JavaScript/function/sort1.js
+++ JavaScript/function/sort1.js
@@ -3,4 +3,8 @@ let ans = num.sort()
 console.log(ans) //[1,12,2,3,4,6,7,9]
 //sort function sort the array numbers using the unicode 
 
-
+let num = [2,5,3,8,6,1];
+let ans = num.sort(function(a, b){
+  return a - b;
+});
+console.log(ans) //[ 1, 2, 3, 5, 6, 8 ]
