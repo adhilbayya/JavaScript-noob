02f90b09b316bedbf59b8b021bd55ba0630a7cdc
AdhilMohammed <adhilvk445@gmail.com>
2023-10-19 23:44:14 +0530

    Create promise-catch.js

---
+++ JavaScript/function/promise-catch.js
@@ -0,0 +1,19 @@
+let success = false;
+
+function getUser(){
+  return new Promise((resolve, reject) =>{
+    setTimeout(() => {
+      if(success){
+        resolve([
+          {username: 'john', email: 'john@test.com' },
+          { username: 'jane', email: 'jane@test.com'}
+        ])
+      }else{
+        reject('failed to the user list');
+      }
+    },1000)
+  })
+}
+getUser().catch((error) => {
+  console.log(error) // failed to the user list
+});
