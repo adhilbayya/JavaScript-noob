acb0fde91f88184fdd0690b6504aed9cd8a7519a
AdhilMohammed <adhilvk445@gmail.com>
2023-10-19 22:38:50 +0530

    Update callback.js

--- JavaScript/function/callback.js
+++ JavaScript/function/callback.js
@@ -3,14 +3,15 @@ function getUser(callback){
     callback([
       {name : 'Adhil', email : 'Adilkkii@123.com'},
       {name : 'Faris', email : 'farsikki1221@123.com'};
-  ]), 1000});
+  ]);
+  }, 1000);
 }
 
 function findUser(username, callback){
   getUser((users) => {
     let user = users.find((user) => user.name === username);
+    callback(user);
   });
-  callback(user);
 }
 findUser('Faris', console.log)
           
