91ee3c165eb6ba2eaa524e4c245c463c03406b77
AdhilMohammed <adhilvk445@gmail.com>
2023-10-20 15:28:18 +0530

    Update promise-chaining.js

--- JavaScript/object/promise-chaining.js
+++ JavaScript/object/promise-chaining.js
@@ -16,3 +16,25 @@ p.then((result) => {
     console.log(result);
     return result * 4
   })
+
+// Other way
+function generateNum(num){
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(num);
+    }, 1000)
+  })
+
+  generateNum(10)
+  .then((res) => {
+    console.log(res);
+    return generateNum(res * 2);
+  })
+  .then((res) => {
+    console.log(res);
+    return generateNum(res * 3);
+  })
+  .then((res) => {
+    console.log(res);
+    return generateNum(res * 4);
+  })
