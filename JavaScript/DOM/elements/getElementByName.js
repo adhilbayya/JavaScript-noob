e43aa981a2cd5f1f88da66ef8186bf4d5f7c8a83
AdhilMohammed <adhilvk445@gmail.com>
2023-10-24 15:12:53 +0530

    Update getElementByName.js

--- JavaScript/DOM/elements/getElementByName.js
+++ JavaScript/DOM/elements/getElementByName.js
@@ -2,8 +2,10 @@ let btn = document.getElementById('btn');
 let message = document.getElementById('message');
 
 btn.addEventListener('click', () => {
-  let rates = document.getElementByName('rate');
+  let rates = document.getElementsByName('rate');
   rates.forEach((rate) => {
+    if(rate.checked){
     message.innerText = `You selected ${rate.value}`;
+    }
   })
 })
